ac254af44bb059f410b1c3da854d954e07a185c7
wairos <wairelm@gmail.com>
2017-03-27 23:41:53 -0500

    Make.java
    Part of a command line app that lets you print out any scale you desire... at least almost any scale. 
    Method minorScale(String[] key) & others, print the desired scale to the console.
    Have been experimenting with creating a String[]  from these methods as seen in mixolydianScale(String[] key) seems it could be useful to return an array containing the desired scale instead of just printing to console.

---
+++ Make.java
@@ -0,0 +1,41 @@
+package scalemaker;
+
+/**
+ *
+ * @author waire
+ */
+public class Make {
+    
+     public static void minorScale(String[] key) {
+        System.out.println(key[0] +" "+ key[1] +" "+ LowerAndRaise.lowerNote(key[2]) +" "+ key[3] +" "+ key[4] +" "+ LowerAndRaise.lowerNote(key[5]) +" "+ LowerAndRaise.lowerNote(key[6]));
+    }
+    public static void majorScale(String[] key) {
+        System.out.println(key[0] +" "+ key[1] +" "+ key[2] +" "+ key[3] +" "+ key[4] +" "+ key[5] +" "+ key[6]);
+    }
+    public static void dorianScale(String[] key) {
+        System.out.println(key[0] +" "+ key[1] +" "+ LowerAndRaise.lowerNote(key[2]) +" "+ key[3] +" "+ key[4] +" "+ key[5] +" "+ LowerAndRaise.lowerNote(key[6]));
+    }
+    public static void phrygianScale(String[] key) {
+        System.out.println(key[0] +" "+ LowerAndRaise.lowerNote(key[1]) +" "+ LowerAndRaise.lowerNote(key[2]) +" "+ key[3] +" "+ key[4] +" "+ LowerAndRaise.lowerNote(key[5]) +" "+ LowerAndRaise.lowerNote(key[6]));
+    }
+    public static void lydianScale(String[] key) {
+        System.out.println(key[0] +" "+ key[1] +" "+ key[2] +" "+ LowerAndRaise.raiseNote(key[3]) +" "+ key[4] +" "+ key[5] +" "+ key[6]);
+    }
+    public static void mixolydianScale(String[] key) {
+        String scale = (key[0] +" "+ key[1] +" "+ key[2] +" "+ key[3] +" "+ key[4] +" "+ key[5] +" "+ LowerAndRaise.lowerNote(key[6]));
+        System.out.println(scale);
+    }
+     public static void harmonicMinorScale(String[] key) {
+        System.out.println(key[0] +" "+ key[1] +" "+ LowerAndRaise.lowerNote(key[2]) +" "+ key[3] +" "+ key[4] +" "+ LowerAndRaise.lowerNote(key[5]) +" "+ key[6]);
+    }
+      public static void locrianScale(String[] key) {
+        System.out.println(key[0] +" "+ LowerAndRaise.lowerNote(key[1]) +" "+ LowerAndRaise.lowerNote(key[2]) +" "+ key[3] +" "+ LowerAndRaise.lowerNote(key[4]) +" "+ LowerAndRaise.lowerNote(key[5]) +" "+ LowerAndRaise.lowerNote(key[6]));
+    }
+      public static void minorPentatonicScale(String[] key) {
+          System.out.println( key[0] +" "+ LowerAndRaise.lowerNote(key[2]) +" "+ key[3] +" "+ key[4] +" "+ LowerAndRaise.lowerNote(key[6]));
+      }
+      public static void majorPentatonicScale(String[] key){
+          System.out.println( key[0] +" "+ key[1] +" "+ key[2] +" "+ key[4] +" "+ key[5]);
+      }
+    
+}
